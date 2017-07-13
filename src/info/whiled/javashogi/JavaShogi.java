ec42fb08cb72e51b96195d7baed7c9136621e9cc
whileD <whiled.tech@gmail.com>
2017-07-14 00:05:17 +0900

    さわりだけ

---
+++ src/info/whiled/javashogi/JavaShogi.java
@@ -0,0 +1,50 @@
+package info.whiled.javashogi;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by whiled on 17/07/13.
+ */
+public class JavaShogi extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    private static final int CELL_SIZE = 30;
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group gridGroup = new Group();
+
+
+        //Gridの生成
+        IntStream.range(0,9).boxed().forEach(i ->
+            IntStream.range(0,9).boxed().forEach(j ->{
+                Rectangle rectangle = new Rectangle(i*CELL_SIZE, j*CELL_SIZE, CELL_SIZE,CELL_SIZE);
+                rectangle.setFill(Color.BLACK);
+                rectangle.setStroke(Color.GRAY);
+                rectangle.setOnMouseExited(mouseEvent -> {
+                    rectangle.setFill(Color.WHITE);
+                });
+                gridGroup.getChildren().add(rectangle);
+            })
+        );
+
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(gridGroup);
+
+        Scene scene = new Scene(borderPane);
+        stage.setScene(scene);
+        stage.setWidth(400);
+        stage.setHeight(300);
+        stage.show();
+    }
+}
